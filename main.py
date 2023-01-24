30697731b85a05290102aafabd2949de3adedd21
Roger <rogeliocrea@gmail.com>
2023-01-24 03:25:39 +0000

    ux improvements

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import tkinter as tk
 from tkinter import font
 
+
 def convert_altitude():
-    altitude = float(entry_altitude.get())
-    input_unit = variable_input.get()
-    output_unit = variable_output.get()
+  altitude = float(entry_altitude.get())
+  input_unit = variable_input.get()
+  output_unit = variable_output.get()
 
-    if input_unit == "km":
-        altitude = altitude * 1000
-    elif input_unit == "ft":
-        altitude = altitude * 0.3048
-    elif input_unit == "m":
-        pass
-    else:
-        result = "Invalid input unit. Please enter km, ft, or m."
+  if input_unit == "km":
+    altitude = altitude * 1000
+  elif input_unit == "ft":
+    altitude = altitude * 0.3048
+  elif input_unit == "m":
+    pass
+  else:
+    result = "Invalid input unit. Please enter km, ft, or m."
 
-    if output_unit == "km":
-        result = altitude / 1000
-    elif output_unit == "ft":
-        result = altitude / 0.3048
-    elif output_unit == "m":
-        result = altitude
-    else:
-        result = "Invalid output unit. Please enter km, ft, or m."
+  if output_unit == "km":
+    result = altitude / 1000
+  elif output_unit == "ft":
+    result = altitude / 0.3048
+  elif output_unit == "m":
+    result = altitude
+  else:
+    result = "Invalid output unit. Please enter km, ft, or m."
 
-    label_result.config(text=result)
+  label_result.config(text=result)
+
 
 root = tk.Tk()
 root.title("Altitude Converter")
@@ -48,18 +50,22 @@ unit_output_dropdown = tk.OptionMenu(root, variable_output, *unit_options)
 unit_input_dropdown.config(font=custom_font)
 unit_output_dropdown.config(font=custom_font)
 
-
+label_from = tk.Label(root, text="from:", font=custom_font)
+label_convert_to = tk.Label(root, text="convert to:", font=custom_font)
 
 label_result = tk.Label(root, text="", font=custom_font)
 
-convert_button = tk.Button(root, text="Convert", command=convert_altitude, font=custom_font)
+convert_button = tk.Button(root,
+                           text="Convert",
+                           command=convert_altitude,
+                           font=custom_font)
 
 # grid layout
-label_altitude.grid(row=0, column=0, pady=10)
-entry_altitude.grid(row=0, column=1, pady=10)
-unit_input_dropdown.grid(row=1, column=0, pady=10)
-unit_output_dropdown.grid(row=1, column=1, pady=10)
-convert_button.grid(row=2, column=0, columnspan=2, pady=10)
-label_result.grid(row=3, column=0, columnspan=2, pady=10)
-
-root.mainloop()
+label_from.grid(row=0, column=0, pady=10)
+unit_input_dropdown.grid(row=0, column=1, pady=10)
+label_altitude.grid(row=1, column=0, pady=10)
+entry_altitude.grid(row=1, column=1, pady=10)
+label_convert_to.grid(row=2, column=0, pady=10)
+unit_output_dropdown.grid(row=2, column=1, pady=10)
+convert_button.grid(row=3, column=0, columnspan=2, pady=10)
+label_result.grid(row=4, column=0, columnspan=2, pady=10)
